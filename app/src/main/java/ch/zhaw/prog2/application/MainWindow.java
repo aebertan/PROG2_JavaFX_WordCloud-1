6b721f0c5b5110e1c89b76855971a929d059efce
Matthias von Arx <vonarmat@students.zhaw.ch>
2022-03-17 14:33:06 +0100

    workshop until schritt 6

--- app/src/main/java/ch/zhaw/prog2/application/MainWindow.java
+++ app/src/main/java/ch/zhaw/prog2/application/MainWindow.java
@@ -1,13 +1,24 @@
 package ch.zhaw.prog2.application;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
 
 public class MainWindow extends javafx.application.Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         openMainWindow(primaryStage);
     }
-    private void openMainWindow(Stage stage){
+    private void openMainWindow(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+        Pane rootNode = loader.load();
+        Scene scene = new Scene(rootNode);
+        stage.setScene(scene);
+        stage.show();
+
 
     }
 
---
+++ app/src/main/java/ch/zhaw/prog2/application/MainWindowController.java
@@ -0,0 +1,26 @@
+package ch.zhaw.prog2.application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class MainWindowController {
+
+    @FXML
+    private TextField textEingabe;
+
+    @FXML
+    private TextArea textHistory;
+
+    @FXML
+    void hinzufuegenText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void leerenTextEingabe(ActionEvent event) {
+
+    }
+
+}
