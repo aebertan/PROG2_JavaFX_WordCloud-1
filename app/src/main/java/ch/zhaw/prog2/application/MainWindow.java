4098b4114661e95a0786f4fd930d11267a9001c4
Matthias von Arx <vonarmat@students.zhaw.ch>
2022-03-17 12:43:42 +0100

    gradle project created

---
+++ app/src/main/java/ch/zhaw/prog2/application/MainWindow.java
@@ -0,0 +1,12 @@
+package ch.zhaw.prog2.application;
+
+import javafx.stage.Stage;
+
+public class MainWindow extends javafx.application.Application{
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+
+}
